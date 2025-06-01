69a3c6fbbc513eac7955a2e7305e656ccd7f96e6
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-06-01 23:48:30 +0330

    TIP__TAP__REACT

--- store/arrayStore.ts
+++ store/arrayStore.ts
@@ -5,12 +5,16 @@ interface ArrayStore {
   array: string[];
   add: (item: string) => void;
   remove: (item: string) => void;
+  text: string;
+  setText: (text: string) => void;
 }
 
 const useArrayStore = create<ArrayStore>()(
   persist(
     (set) => ({
       array: ["ALI", "REZA", "MAHDI"],
+      text: "HELLO WORLD",
+      setText: (text) => set({ text }),
       add: (item) => set((state) => ({ array: [...state.array, item] })),
       remove: (item) =>
         set((state) => ({ array: state.array.filter((i) => i !== item) })),

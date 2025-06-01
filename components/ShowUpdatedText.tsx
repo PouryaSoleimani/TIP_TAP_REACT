1646a559dcc603a59f7bab241727b951af298296
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-06-02 00:35:47 +0330

    TIP__TAP__REACT

--- components/ShowUpdatedText.tsx
+++ components/ShowUpdatedText.tsx
@@ -1,18 +1,20 @@
 "use client"
 import useArrayStore from '@/store/arrayStore'
 import React from 'react'
-import { Code, Flex } from "@chakra-ui/react"
+import { Button, Code, Flex } from "@chakra-ui/react"
 
 
 const ShowUpdatedText = () => {
     const { text } = useArrayStore()
 
     return (
-        <div>
+        <div className='flex flex-col gap-y-6'>
             <Code className='!text-3xl font-bold !p-5 !my-6'>UPDATED TEXT</Code>
-            <Flex className='bg-zinc-900 p-3 rounded-lg text-xl font-semibold mt-4'>
+            <Flex className='bg-zinc-900 p-3 rounded-lg text-xl font-semibold mt-4 list-disc'>
                 <p className='text-2xl font-bold text-center !p-10 !border' dangerouslySetInnerHTML={{ __html: text }}></p>
             </Flex>
+            <hr />
+            <Button color={"white"} backgroundColor={"darkred"} onClick={() => console.info("TEXT", text)}>SUBMIIT</Button>
         </div>
     )
 }
--- components/TipTapEditor.tsx
+++ components/TipTapEditor.tsx
@@ -4,7 +4,9 @@ import { useEditor, EditorContent } from '@tiptap/react'
 import useArrayStore from '@/store/arrayStore'
 import Document from '@tiptap/extension-document'
 import Heading from '@tiptap/extension-heading'
+import ListItem from '@tiptap/extension-list-item'
 import Paragraph from '@tiptap/extension-paragraph'
+import BulletList from '@tiptap/extension-bullet-list'
 import Text from '@tiptap/extension-text'
 import { Button, Flex } from '@chakra-ui/react'
 
@@ -12,7 +14,7 @@ const TipTapEditor = () => {
     const { text, setText } = useArrayStore()
 
     const editor = useEditor({
-        extensions: [Document, Paragraph, Text, Heading.configure({ levels: [1, 2, 3, 4, 5, 6], }),],
+        extensions: [Document, Paragraph, Text, ListItem, BulletList, Heading.configure({ levels: [1, 2, 3, 4, 5, 6], }),],
         content: text,
         onUpdate: ({ editor }) => {
             const content = editor.getHTML()
@@ -27,6 +29,12 @@ const TipTapEditor = () => {
         <>
             <div className="control-group">
                 <Flex className="button-group space-x-4">
+                    <button
+                        onClick={() => editor.chain().focus().toggleBulletList().run()}
+                        className={editor.isActive('bulletList') ? 'is-active' : ''}
+                    >
+                        Toggle bullet list
+                    </button>
                     <Button onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()} style={{ margin: "0 1rem" }} className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''} >        H1    </Button>
                     <Button onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()} style={{ margin: "0 1rem" }} className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''} >     H2 </Button>
                     <Button onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()} style={{ margin: "0 1rem" }} className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}  >     H3 </Button>
